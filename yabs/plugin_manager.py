ad0a846f9a5f396407d725f9860f3a5bcf5771ea
Martin Wendt <github@wwwendt.de>
2020-07-20 21:34:33 +0200

    Update plugin_manager.py

--- yabs/plugin_manager.py
+++ yabs/plugin_manager.py
@@ -16,7 +16,7 @@ from .cmd_gh_release import GithubReleaseTask
 from .cmd_push import PushTask
 from .cmd_pypi_release import PypiReleaseTask
 from .cmd_tag import TagTask
-from .util import log_info, log_warning, logger
+from .util import log_debug, log_info, log_warning, logger
 
 # from semantic_version import Version
 
@@ -31,7 +31,7 @@ class PluginManager:
     #: (str) Entry point group name
     namespace = "yabs.tasks"
     #: (bool) True when all plugin entry points are loaded
-    entry_points_loaded = False
+    entry_points_searched = False
     #: (bool) True when all plugins' 'register()' method were called.
     plugins_registered = False
     #: (dict) Cached map TASK_NAME => EntryPoint of loaded entry_points
@@ -53,22 +53,24 @@ class PluginManager:
         pass
 
     @classmethod
-    def load_plugins(cls):
-        """Load all entry points wit group name 'yabs.tasks'."""
-        if cls.entry_points_loaded:
+    def find_plugins(cls):
+        """Load all entry points with group name 'yabs.tasks'."""
+        if cls.entry_points_searched:
             return
-        cls.entry_points_loaded = True
+        cls.entry_points_searched = True
         ep_map = cls._entry_point_map
-        log_info("Load plugins for namespace '{}'...".format(cls.namespace))
+        log_debug("Search entry points for group '{}'...".format(cls.namespace))
 
         for ep in iter_entry_points(group=cls.namespace, name=None):
-            log_info("Load entry point '{}' {}...".format(ep.name, ep.module_name))
+            plugin_name = "{}".format(ep.dist)
+            log_debug("Found plugin {} from entry point `{}`".format(plugin_name, ep))
+
             if ep.name in ep_map:
                 log_warning(
                     "Duplicate entry point name: {}; skipping...".format(ep.name)
                 )
                 continue
-            if ep.name in cls.task_class_map:
+            elif ep.name in cls.task_class_map:
                 # TODO: support overriding standard tasks?
                 # Maybe when 'exreas=[override]' is passed...
                 log_warning(
@@ -76,14 +78,8 @@ class PluginManager:
                 )
                 continue
 
-            try:
-                register_fn = ep.load()
-                if not callable(register_fn):
-                    raise RuntimeError("Entry point {} is not a function".format(ep))
-                ep_map[ep.name] = register_fn
-            except Exception:
-                logger.exception("Failed to load {}".format(ep))
-        # log_info("map: {}".format(ep_map))
+            ep_map[ep.name] = ep
+
         return
 
     @classmethod
@@ -91,10 +87,21 @@ class PluginManager:
         if cls.plugins_registered:
             return
         cls.plugins_registered = True
-        cls.load_plugins()
 
-        for name, register_fn in cls._entry_point_map.items():
-            log_info("Register plugin '{}.{}'...".format(cls.namespace, name))
+        cls.find_plugins()
+
+        ep_map = cls._entry_point_map
+        for name, ep in cls._entry_point_map.items():
+            log_info("Load plugin {}...".format(ep.dist))
+            try:
+                register_fn = ep.load()
+                if not callable(register_fn):
+                    raise RuntimeError("Entry point {} is not a function".format(ep))
+                ep_map[ep.name] = register_fn
+            except Exception:
+                logger.exception("Failed to load {}".format(ep))
+
+            log_debug("Register plugin {}...".format(ep.dist))
             try:
                 plugin = register_fn(task_base=WorkflowTask)
             except Exception:
