4d0a1b09c2e8fd51d09286b4962d1909dd0d6e12
Martin Wendt <github@wwwendt.de>
2023-02-23 21:20:59 +0100

    furo

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -227,12 +227,13 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-if not on_rtd:
-    # only import and set the theme if we're building docs locally
-    # otherwise, readthedocs.org uses their theme by default, so no need to specify it
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "furo"
+# if not on_rtd:
+#     # only import and set the theme if we're building docs locally
+#     # otherwise, readthedocs.org uses their theme by default, so no need to specify it
+#     import sphinx_rtd_theme
+#     html_theme = 'sphinx_rtd_theme'
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
